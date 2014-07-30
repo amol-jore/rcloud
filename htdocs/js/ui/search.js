9e91ccde8e941cd6666db48a6bfa2e114e62f98a
amol-jore <vagrant@precise32.(none)>
2014-07-30 16:11:05 +0000

    changed the comment properties delimiter to :::

--- htdocs/js/ui/search.js
+++ htdocs/js/ui/search.js
@@ -55,8 +55,8 @@ RCloud.UI.search = {
                                     parts_table += "<tr><th class='search-result-part-name'>" + d[i].parts[k].filename + "</th></tr>";
                                 for(var l = 0; l < content.length; l++) {
                                     if (d[i].parts[k].filename === "comments") {
-                                        var comment_content = content[l].substr(content[l].indexOf(":")+1, content[l].lastIndexOf(":")-content[l].indexOf(":")-1);
-                                        var comment_author = content[l].substr(content[l].lastIndexOf(":")+1, content[l].length-content[l].lastIndexOf(":")-1);
+                                        var comment_content = content[l].substr(content[l].indexOf(":::")+3, content[l].lastIndexOf(":::")-content[l].indexOf(":::")-3);
+                                        var comment_author = content[l].substr(content[l].lastIndexOf(":::")+3, content[l].length-content[l].lastIndexOf(":::")-3);
                                         inner_table += "<tr><td width='auto'><b>" + comment_author + "</b>&nbsp;&nbsp;</td><td class='search-result-code'><i>" + comment_content + "</i></td></tr>";
                                     }
                                     else {
